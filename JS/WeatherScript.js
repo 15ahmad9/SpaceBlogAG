a18cc3c0bb30c5e986c1d98e85a07ad7c278643e
Ahmad Ghanem <109623987+15ahmad9@users.noreply.github.com>
2023-02-05 15:04:33 +0300

    Add files via upload

---
+++ JS/WeatherScript.js
@@ -0,0 +1,45 @@
+// d
+let weather = {
+  apiKey: "faa25aa6a10c047d58b40c96b2ced13",
+  fetchWeather: function (city) {
+    fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=" + this.apiKey)
+      .then((response) => {
+        if (!response.ok) {
+          alert("No weather detected in this area.");
+          throw new Error("No weather detected in this area.");
+        }
+        return response.json();
+      })
+      .then((data) => this.displayWeather(data));
+  },
+
+  displayWeather: function (data) {
+    const { name } = data;
+    const { icon, description } = data.weather[0];
+    const { temp, humidity } = data.main;
+    const { speed } = data.wind;
+    document.querySelector(".city").innerText = "Weather in " + name;
+    document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
+    document.querySelector(".description").innerText = description;
+    document.querySelector(".temp").innerText = temp + "°C";
+    document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
+    document.querySelector(".wind").innerText = "Wind speed: " + speed + " km/h";
+    document.querySelector(".weather").classList.remove("loading");
+  },
+
+  search: function () {
+    this.fetchWeather(document.querySelector(".search-bar").value);
+  },
+};
+
+document.querySelector(".search button").addEventListener("click", function () {
+  weather.search();
+});
+
+document.querySelector(".search-bar").addEventListener("keyup", function (event) {
+  if (event.key == "Enter") {
+    weather.search();
+  }
+});
+
+weather.fetchWeather("Denver");
---
+++ JS/script.js
@@ -0,0 +1,57 @@
+// c
+// let searchButton = document.querySelector("#search")
+
+// searchButton.addEventListener("click", ()=>{
+//   request()
+// })
+
+// async function request(){
+//   let API_KEY = "ba4DQkxyfYCyjplegsrZ81qostNBrDAP100hrMtJ"
+//   let response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`);
+//   console.log(response)
+
+//   let data = await response.json()
+//   console.log(data)
+
+//   showData(data)
+// }
+
+// function showData(data){
+//     document.querySelector("#content").innerHTML += data.explanation
+//     document.querySelector("#content").innerHTML += `<img src = "${data.url}">`
+// }
+
+const input = document.querySelector("input.bday-date");
+
+const date = new Date().toLocaleDateString("en-ca");
+input.max = date;
+
+input.addEventListener("change", (event) => getAPOD(event.target.value));
+
+const getAPOD = (date) => {
+  const API_KEY = "jeedZalahZm8ZxpF8fAdRE4w0GLLWovZVFUK3M2";
+  fetch(`https://api.nasa.gov/planetary/apod?date=${date}&api_key=${API_KEY}`)
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error("Bad server response");
+    })
+    .then((data) => displayAPOD(data))
+    .catch(() => displayNotFound());
+};
+
+const displayAPOD = (data) => {
+  const apodCard = document.querySelector(".apod");
+  const apodCard2 = document.querySelector(".apod2");
+  apodCard.innerHTML = `<h3 class="apod-title">${data.title}</h3>
+    
+    <img src=${data.url} title=${data.title} class="apod-pic" />`;
+  apodCard2.innerHTML = '<h3 class="apod-title">Explane</h3>';
+  apodCard2.innerHTML += data.explanation
+};
+
+const displayNotFound = () => {
+  const apodCard = document.querySelector(".apod");
+  apodCard.innerHTML = '<h3 class="apod-title">On this day, no picture was discovered.<</h3>';
+};
